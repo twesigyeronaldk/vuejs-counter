7837a0311e80fc9c4cd880c9ee96832cb73a008b
Twesigye Ronald Karakire <twesigyeronaldk@gmail.com>
2024-07-27 21:11:11 +0300

    Simple vue js counter app

---
+++ Vue/VueCounterApp/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import {ref} from "vue";
+
+  const count = ref(0);
+
+  const addToCount = () => {
+    count.value = count.value + 1
+  }
+
+  const subtractFromCount = () => {
+    count.value = count.value - 1
+  }
+</script>
+-->
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount">-</button>
+      <button @click="addToCount">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main{
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div{
+  text-align: center;
+}
+</style>
